9fddbc221423bc52346ae89626a801a6e01da299
nan308@naver.com <nan308@naver.com>
2020-06-20 21:42:07 +0900

    1377 버블소트 해결

---
+++ 1377 버블소트.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = [int(input()) for i in range(n)]
+
+# chage = 0
+
+# for i in range(n):
+#   chage = 0
+#   for j in range(n-i-1):
+#     if arr[j] > arr[j+1]:
+#       chage = 1
+#       arr[j], arr[j+1] = arr[j+1], arr[j]
+#   if chage == 0:
+#     print(i+1)
+#     break
+
