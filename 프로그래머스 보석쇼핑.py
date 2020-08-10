8b74ac172f31903c5d973dc60d2f997f3bf0ad31
KwonEyng <nan308@naver.com>
2020-08-11 03:58:33 +0900

    Solve 보석쇼핑 (완)

--- 프로그래머스 보석쇼핑.py
+++ 프로그래머스 보석쇼핑.py
@@ -1,31 +1,26 @@
-from collections import defaultdict
-
-
 def solution(gems):
-    result = [0, len(gems)]
-    gem_kinds = len(set(gems))
-    count_dict = defaultdict(int)
-    count_dict[gems[0]] = 1
-
-    left_idx, right_idx = 0, 0
-    while right_idx < len(gems) and left_idx < len(gems):
-        print(count_dict)
-        if len(count_dict) == gem_kinds:
-            if result[1] - result[0] > right_idx - left_idx:
-                result = [left_idx + 1, right_idx + 1]
-
-            count_dict[gems[left_idx]] -= 1
-            if count_dict[gems[left_idx]] == 0:
-                del count_dict[gems[left_idx]]
-            left_idx += 1
-
+    answer = [0,100000]
+    total = len(set(gems))
+    ht = {}
+    start = 0
+    for i in range(len(gems)):
+        gem = gems[i]
+        if ht.get(gem):
+            ht[gem]+=1
         else:
-            right_idx += 1
-            if right_idx == len(gems):
-                break
-            count_dict[gems[right_idx]] += 1
-
-    return result
+            ht[gem]=1
+        if len(ht) == total:
+            while start<=i:
+                if ht[gems[start]]-1>0:
+                    ht[gems[start]] -= 1
+                    start += 1
+                elif answer[1]-answer[0] > i-start:
+                    answer = [start+1,i+1]
+                    break
+                else :
+                    break
+        
+    return answer
 
 gems = ["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]
 solution(gems)
