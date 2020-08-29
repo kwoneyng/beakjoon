efc523257e6484dd74c2bb3ab34b0aeb1da94ec7
KwonEyng <nan308@naver.com>
2020-08-29 18:31:24 +0900

    Solve 가장 먼 노드

--- 가사검색.py
+++ 가사검색.py
@@ -1,35 +1,64 @@
-class Node:
-    def __init__(self, key, data=None):
-        self.key = key
-        self.data = data
-        self.children = {}
+from collections import deque
 
+class Node:
+    def __init__(self, char):
+        self.char = char
+        self.count = 1
+        self.child = {}
+    
 class Trie:
     def __init__(self):
-        self.head = Node(None)
+        self.ht = {}
+        self.rht = {}
 
-    def insert(self, string):
-        curr_node = self.head
-
-        for c in string:
-            if c not in curr_node.children:
-                curr_node.children[c] = Node(c)
-
-            curr_node = curr_node.children[c]
-        
-        curr_node.data = string
-    
-    def search(self, string):
-        curr_node = self.head
-
-        for char in string:
-            if char in curr_node.children:
-                curr_node = curr_node.children[char]
+    def insert(self, string, length):
+        q = deque(list(string))
+        rq = deque(reversed(list(string)))
+        if not self.ht.get(length):
+            self.ht[length] = Node(None)
+            self.rht[length] = Node(None)
+        head = self.ht[length]
+        rhead = self.rht[length]
+        while q:
+            c = q.popleft()
+            rc = rq.popleft()
+            if head.child.get(c):
+                head.child[c].count += 1 # Node
+            else: 
+                head.child[c] = Node(c)
+            if rhead.child.get(rc):
+                rhead.child[rc].count += 1
             else:
-                return False
+                rhead.child[rc] = Node(rc)
+            head = head.child[c]
+            rhead = rhead.child[rc]
+    
+    def search(self,query,length):
+        if self.ht.get(length):
+            head = self.ht[length]
+        else:
+            return 0
+        if query[0] == '?':
+            query = query[::-1]
+            head = self.rht[length]
+        query = query.replace('?','')
+        for i in query:
+            if head.child.get(i):
+                head = head.child[i]
+            else:
+                return 0
+        return head.count
+            
+
 
 def solution(words, queries):
     answer = []
+    trie = Trie()
+    for w in words:
+        trie.insert(w, len(w))
+        
+    for q in queries:
+        answer.append(trie.search(q,len(q)))
     return answer
 
                 
